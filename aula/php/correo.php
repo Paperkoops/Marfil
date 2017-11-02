780d8dc29e93d18c1b46e0848176f06f0e65394a
unknown <hokumageo@gmail.com>
2017-11-02 13:17:28 -0600

    Entrar

--- aula/php/correo.php
+++ aula/php/correo.php
@@ -1,17 +1,17 @@
 <?php   
-include '../vendors/phpmailer/PHPMailerAutoload.php';
+include 'phpmailer/PHPMailerAutoload.php';
 $mail = new PHPMailer;
-$mail>-isSMTP();
+$mail->isSMTP();
 $mail->Debugoutput = 'html';
 $mail->Host = "smtp.gmail.com";
-$mail->Port = 465;
+$mail->Port = 587;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure  = 'ssl';
 $mail->Username = "hokumageo@gmail.com";
 $mail->Password = "22357237Pachi";
 $mail->setFrom('hokumageo@gmail.com','Brandon Giovanni');
 $mail->addReplyTo('hokumageos@gmail.com','Yo alterno');
-$mail->addAddress('hokumageos@gmail.com','Yo alterno')
+$mail->addAddress('hokumageos@gmail.com','Yo alterno');
 $mail->Subject = 'Prueba de correo UwU';
 $mail->Body = "Este es un mensaje de prueba <h1>En teoria es full html nativo</h1>";
 $mail->AltBody = "Hola OWO";
